4e16c2fa0d95c0513c0f7c37e18b9ac0d0e3a823
FaxuanLuo <88180662+luofx23@users.noreply.github.com>
2025-05-13 19:47:18 +0800

    Update solver.py

--- src/janc/solver/solver.py
+++ src/janc/solver/solver.py
@@ -5,18 +5,18 @@ from .flux import weno5
 from ..thermodynamics import thermo
 from ..thermodynamics import chemical
 from ..boundary import boundary
-from ..grid import read_grid as gd
+#from ..grid import read_grid as gd
 from functools import partial
 
 
 
-def CFL(field,cfl=0.20):
+def CFL(field,dx,dy,dz,cfl=0.20):
     U, aux = field[0:-2],field[-2:]
     _,u,v,w,_,_,a = aux_func.U_to_prim(U,aux)
     cx = jnp.max(abs(u) + a)
     cy = jnp.max(abs(v) + a)
     cz = jnp.max(abs(w) + a)
-    dt = jnp.minimum(cfl*gd.dxi/cx,cfl*gd.deta/cy,cfl*gd.ddelta/cz)
+    dt = jnp.minimum(cfl*dx/cx,cfl*dy/cy,cfl*dz/cz)
     return dt
 
 def set_solver(thermo_set, boundary_set, source_set = None, nondim_set = None):
@@ -24,19 +24,19 @@ def set_solver(thermo_set, boundary_set, source_set = None, nondim_set = None):
     boundary.set_boundary(boundary_set)
     aux_func.set_source_terms(source_set)
     
-    def rhs(U,aux,theta=None):
+    def rhs(U,aux,dx,dy,dz,theta=None):
         aux = aux_func.update_aux(U, aux)
         U_with_ghost,aux_with_ghost = boundary.boundary_conditions(U,aux,theta)
-        physical_rhs = weno5(U_with_ghost,aux_with_ghost) + aux_func.source_terms(U, aux, theta)
+        physical_rhs = weno5(U_with_ghost,aux_with_ghost,dx,dy,dz) + aux_func.source_terms(U, aux, theta)
         return physical_rhs
     
 
-    def advance_flux(field,dt,theta=None):
+    def advance_flux(field,dt,dx,dy,dz,theta=None):
         
         U, aux = field[0:-2],field[-2:]
-        U1 = U + dt * rhs(U,aux,theta)
-        U2 = 3/4*U + 1/4 * (U1 + dt * rhs(U1,aux,theta))
-        U3 = 1/3*U + 2/3 * (U2 + dt * rhs(U2,aux,theta))
+        U1 = U + dt * rhs(U,aux,dx,dy,dz,theta)
+        U2 = 3/4*U + 1/4 * (U1 + dt * rhs(U1,aux,dx,dy,dz,theta))
+        U3 = 1/3*U + 2/3 * (U2 + dt * rhs(U2,aux,dx,dy,dz,theta))
         field = jnp.concatenate([U3,aux],axis=0)
         
         return field
@@ -56,8 +56,8 @@ def set_solver(thermo_set, boundary_set, source_set = None, nondim_set = None):
 
 
     @jit    
-    def advance_one_step(field,dt,theta=None):
-        field_adv = advance_flux(field,dt,theta)
+    def advance_one_step(field,dt,dx,dy,dz,theta=None):
+        field_adv = advance_flux(field,dt,dx,dy,dz,theta)
         field = advance_source_term(field_adv,dt)
         return field
 
